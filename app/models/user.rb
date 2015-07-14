824bdfff904da24dc40f60a35d188e4fa70cfc80
phillipspc <phillipspc@gmail.com>
2015-07-14 11:00:26 -0400

    Add user model validations and authenticity related methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,31 @@
 
 class User < ActiveRecord::Base
 
+  validates :username, :email, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest) == password
+  end
+
+  def reset_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
 end
