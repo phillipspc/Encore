775cb3985738e7054f6d1fd898fe42efb81763fa
phillipspc <phillipspc@gmail.com>
2015-07-14 13:50:52 -0400

    Add new user view for user creation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      redirect_to new_session_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
 
   def edit
