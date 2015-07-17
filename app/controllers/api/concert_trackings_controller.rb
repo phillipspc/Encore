ae650e2462e8cccb698b54ee4f59a5b512d01cbb
phillipspc <phillipspc@gmail.com>
2015-07-17 16:58:48 -0400

    Create Api:ConcertTrackingsController with create and destroy methods.

--- app/controllers/api/concert_trackings_controller.rb
+++ app/controllers/api/concert_trackings_controller.rb
@@ -1,6 +1,19 @@
 module Api
   class ConcertTrackingsController < ApiController
 
-    
+    def create
+      ConcertTracking.create!({
+        concert_id: params[:concert_id], user_id: current_user.id
+        })
+      render json: {}
+    end
+
+    def destroy
+      ConcertTracking.find_by!({
+        concert_id: params[:concert_id], user_id: current_user.id
+      }).destroy!
+      render json: {}
+    end
+
   end
 end
