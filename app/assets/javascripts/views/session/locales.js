ae5762dda23a7748dd120f3f836c2ab9ab3590cb
phillipspc <phillipspc@gmail.com>
2015-07-17 15:38:30 -0400

    Add Untrack functionality to Locales via user's locales page.

--- app/assets/javascripts/views/session/locales.js
+++ app/assets/javascripts/views/session/locales.js
@@ -1,26 +1,39 @@
 Encore.Views.SessionLocales = Backbone.View.extend({
   template: JST['session/locales'],
 
+  events: {
+    'click .untrack': 'untrack'
+  },
+
   initialize: function () {
-    this.locales = this.model.locales();
+    var locales = this.model.locales();
     this.listenTo(this.model, 'sync', this.render);
+    this.listenTo(locales, 'remove', this.render)
+  },
+
+  untrack: function (event) {
+    event.preventDefault();
+    $target = $(event.currentTarget);
+    var locales = this.model.locales();
+    var locale = locales.get($target.attr('data-id'));
+    locale.untrack(locales);
   },
 
   render: function () {
-      var content = this.template({
-        locales: this.locales
-      });
-      this.$el.html(content);
+    var locales = this.model.locales();
+    var content = this.template({
+      locales: locales
+    });
+    this.$el.html(content);
 
-      var that = this;
-      var locales = this.locales;
-      locales.each( function (locale){
-        locales.getOrFetch(locale.id);
-        var view = new Encore.Views.SessionLocale({
-          model: locale
-        });
-        that.$el.append(view.render().$el);
+    var that = this;
+    locales.each( function (locale){
+      locales.getOrFetch(locale.id);
+      var view = new Encore.Views.SessionLocale({
+        model: locale
       });
-      return this;
+      that.$el.append(view.render().$el);
+    });
+    return this;
   }
 });
