a0712a185c1309e728be9fb0388a724241be60fa
phillipspc <phillipspc@gmail.com>
2015-07-20 12:21:18 -0400

    Refactor User Locations page.

--- app/assets/javascripts/views/session/locale.js
+++ app/assets/javascripts/views/session/locale.js
@@ -1,9 +1,30 @@
 Encore.Views.SessionLocale = Backbone.View.extend({
   template: JST['session/locale'],
 
+  events: {
+    'click .track-locale' : 'trackLocale',
+    'click .untrack-locale' : 'untrackLocale'
+  },
+
+  initialize: function () {
+    this.listenTo(this.model, 'sync', this.render);
+  },
+
+  trackLocale: function () {
+    event.preventDefault();
+    this.model.track(this.collection);
+  },
+
+  untrackLocale: function () {
+    event.preventDefault();
+    this.model.untrack(this.collection);
+  },
+
   render: function () {
+    var ids = this.collection.pluck('id');
     var content = this.template({
-      locale: this.model
+      locale: this.model,
+      ids: ids
     });
     this.$el.html(content);
     return this;
--- app/assets/javascripts/views/session/locales.js
+++ app/assets/javascripts/views/session/locales.js
@@ -1,36 +1,22 @@
 Encore.Views.SessionLocales = Backbone.View.extend({
   template: JST['session/locales'],
 
-  events: {
-    'click .untrack-locale': 'untrack'
-  },
-
   initialize: function () {
-    var locales = this.model.locales();
     this.listenTo(this.model, 'sync', this.render);
-    this.listenTo(locales, 'remove', this.render)
-  },
-
-  untrack: function (event) {
-    event.preventDefault();
-    $target = $(event.currentTarget);
-    var locales = this.model.locales();
-    var locale = locales.get($target.attr('data-id'));
-    locale.untrack(locales);
   },
 
   render: function () {
-    var locales = this.model.locales();
     var content = this.template({
-      locales: locales
+      locales: this.model.locales()
     });
     this.$el.html(content);
 
     var that = this;
+    var locales = this.model.locales();
     locales.each( function (locale){
-      locales.getOrFetch(locale.id);
       var view = new Encore.Views.SessionLocale({
-        model: locale
+        model: locale,
+        collection: locales
       });
       that.$el.append(view.render().$el);
     });
